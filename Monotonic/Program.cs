3958b7a99281e07f1b483db65de45ba899f4c647
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-11-23 16:08:29 -0500

    added files

---
+++ Monotonic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Monotonic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+        public static bool IsMonotonic(int[] A)
+        {
+            bool inc = true;
+            bool dec = true;
+            for(int i=0;i<A.Length-1;++i)
+            {
+                if(A[i] < A[i+1])
+                {
+                    dec = false;
+                }
+                if (A[i] > A[i + 1])
+                {
+                    inc = false;
+                }
+            }
+            return inc || dec;
+        }
+     }
+}
